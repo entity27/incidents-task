a3ef789e48a1ef255738f8e268c3a8bfb4fe1bc1
entity27 <mailforinform@mail.ru>
2025-04-19 01:54:01 +0300

    fix(alembic): invalid setup & mako renewal

--- alembic/env.py
+++ alembic/env.py
@@ -5,9 +5,8 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from src import autoimport_models
 from src.config.settings import settings
-from src.utils.sqlaclhemy import Base
+from src.config.setup import db
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -22,8 +21,8 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-autoimport_models()
-target_metadata = Base.metadata
+from src.incidents.models import *
+target_metadata = db.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
