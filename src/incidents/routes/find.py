d0a5892c347342a8839ab94490b451d2ffaf121a
entity27 <mailforinform@mail.ru>
2025-04-19 12:50:18 +0300

    feat(routes): find & problems

--- src/incidents/routes/find.py
+++ src/incidents/routes/find.py
@@ -1,11 +1,49 @@
-from typing import Any
+from flask import request
+from flask_restful import Resource, abort, marshal_with
 
-from flask_restful import Resource
+from src.incidents.repositories.incident import IncidentRepository
+from src.incidents.responses.find import find_response
+from src.incidents.schemas.find import find_get_parser
+from src.incidents.services.find import FindService
+from src.utils.responses import HttpResponse
 
 
 class FindResource(Resource):
-    def get(self) -> Any:
-        return {'dummy': 'OK'}, 200
+    def __init__(
+        self, service: FindService = FindService(repo=IncidentRepository())
+    ) -> None:
+        """
+        Ресурс поиска по инцидентам
+        """
+        self._service = service
 
-    def post(self) -> Any:
-        return {'dummy': 'OK'}, 200
+    @marshal_with(find_response)  # type: ignore[misc]
+    def get(self) -> HttpResponse:
+        """
+        Ожидаем query-параметр `h` (хеш), выполняет поиск по хешу
+
+        Returns:
+            Список найденных инцидентов
+        """
+        hashed: str = find_get_parser.parse_args()['h']
+        data = self._service.by_hash(hashed)
+        return {'incidents': data}, 200
+
+    @marshal_with(find_response)  # type: ignore[misc]
+    def post(self) -> HttpResponse:
+        """
+        Ожидаем JSON, выполняет поиск по атрибутам
+
+        Notes:
+            Ищет совпадения по полученным данным в БД среди заголовков и тел
+
+        Returns:
+            Список найденных инцидентов
+        """
+        if not request.is_json:
+            abort(http_status_code=400, message='Данные должны быть в формате JSON')
+
+        values = request.get_json()
+        data = self._service.by_values(values)
+
+        return {'incidents': data}, 200
--- src/incidents/routes/problems.py
+++ src/incidents/routes/problems.py
@@ -1,8 +1,34 @@
-from typing import Any
+from flask import request
+from flask_restful import Resource, abort, marshal_with
 
-from flask_restful import Resource
+from src.incidents.repositories.incident import IncidentRepository
+from src.incidents.responses.problems import problems_response
+from src.incidents.services.problems import ProblemsService
+from src.utils.responses import HttpResponse
 
 
 class ProblemsResource(Resource):
-    def post(self) -> Any:
-        return {'dummy': 'OK'}, 201
+    def __init__(
+        self, service: ProblemsService = ProblemsService(repo=IncidentRepository())
+    ) -> None:
+        """
+        Ресурс создания инцидентов
+        """
+        self._service = service
+
+    @marshal_with(problems_response)  # type: ignore[misc]
+    def post(self) -> HttpResponse:
+        """
+        Принимаем JSON запрос, создаём инцидент с телом и заголовками запроса
+
+        Returns:
+            Хеш инцидента
+        """
+        if not request.is_json:
+            abort(http_status_code=400, message='Данные должны быть в формате JSON')
+
+        headers = dict(request.headers.items())
+        body = request.get_json()
+        data = self._service.create(headers, body)
+
+        return data, 201
